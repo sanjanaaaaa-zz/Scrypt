caaf7e7d41496530efdccab1d202ca1d02cc3b1a
Sanjana Chaudhary <sanjanaec47@gmail.com>
2025-11-27 13:10:07 +0530

    files added

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+      <p>
+        &copy; {new Date().getFullYear()} Scrypt | Created by Sanjana
+        Kumar
+      </p>
+    </footer>
+  );
+};
+export default Footer;
